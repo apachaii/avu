945c1775cc030d76b90c58a4577beb9634406729
apachaii <daniloneko@gmail.com>
2018-01-04 11:02:36 -0300

    se agregaron archivos de configuracion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { ChatService,Message } from './chat.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/scan';
+import {VozService} from './voz.service'
+
 
 @Component({
   selector: 'app-root',
+  providers: [VozService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '';
+
+  formValue: string;
+  constructor(public chat:ChatService,
+    private speech: VozService) { }
+
+    sendMessage(){
+    this.chat.converce(this.formValue);
+    this.formValue = '';
+  }
+   
+   Activar(conf:boolean){
+     console.log(conf);
+     if(conf){
+      this.speech.callar();
+      conf = false;
+     }
+     if(!conf)
+     {
+      this.speech.record('es_MX')
+      .subscribe(e => this.formValue = e)
+     }
+      
+   }
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot: '5824afb8286349cab301685f75f6a6c4'
+  }
+
 };
